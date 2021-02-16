8d6038104339c0c12a25d90da5fdae49c9145d5a
Nishit Khaitan <nishitk@gmail.com>
2021-02-16 15:42:43 +0530

    Initial Flask

---
+++ flask/app.py
@@ -0,0 +1,22 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # return "hey"
+    return render_template('landing.html')
+
+@app.route('/about')
+def about():
+    # return "about"
+    return render_template('about.html')
+
+
+@app.route('/login')
+def login():
+    # return "login"
+    return render_template('login.html')
+
+if __name__ == "__main__" :
+    app.run(debug=True)
